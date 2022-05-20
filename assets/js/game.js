40599fe91c2480941370f1069ea4a19a66df7a5d
mitchgm <mitchgm@bgsu.edu>
2022-05-20 06:59:56 -0700

    added fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,43 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+  window.alert("Welcome to Robot Gladiators!");
+  // calculate the player attack
+    enemyHealth = enemyHealth - playerAttack;
+  // log a message to the console that it worked
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    )
+  // calculate the enemy attack
+        playerHealth = playerHealth - enemyAttack;
+  //log a message to the console that it worked
+        console.log(
+            enemyName + " has attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+        )
+};
+
+fight();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
