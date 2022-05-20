21c5289a989cd7889f6ba55d5319841b47cea925
mitchgm <mitchgm@bgsu.edu>
2022-05-20 07:16:42 -0700

    updated fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,6 +2,7 @@ var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 
+
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
@@ -17,12 +18,25 @@ var fight = function() {
     console.log(
         playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
     )
+  // check enemy health
+  if (enemyHealth <= 0){
+      window.alert(enemyName + " has died!");
+  }  
+  else {
+      window.alert(enemyName + " still has " + enemyHealth + " health remaining.");
+  }
   // calculate the enemy attack
         playerHealth = playerHealth - enemyAttack;
   //log a message to the console that it worked
         console.log(
             enemyName + " has attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
         )
+    if (playerHealth <= 0){
+        window.alert(playerName + " has died!");
+    }    
+    else {
+        window.alert(playerName + " still has" + playerHealth + " health remaining");
+    }
 };
 
 fight();
